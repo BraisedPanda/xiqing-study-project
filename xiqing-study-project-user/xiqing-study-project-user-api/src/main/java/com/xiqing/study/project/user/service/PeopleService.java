a93fc56b1266c8f4017722a4c5fbdaa4bccfe81d
braisedpanda <987433278@qq.com>
2021-12-29 17:34:49 +0800

    代码提交

--- xiqing-study-project-user/xiqing-study-project-user-api/src/main/java/com/xiqing/study/project/user/service/PeopleService.java
+++ xiqing-study-project-user/xiqing-study-project-user-api/src/main/java/com/xiqing/study/project/user/service/PeopleService.java
@@ -1,6 +1,7 @@
 package com.xiqing.study.project.user.service;
 
 import com.xiqing.study.project.base.model.DataResponseBean;
+import com.xiqing.study.project.base.model.SimpleResponseBean;
 import com.xiqing.study.project.domain.po.People;
 
 import java.util.List;
@@ -11,4 +12,16 @@ public interface PeopleService {
     DataResponseBean<List<People>> selectAll();
 
     DataResponseBean<People> selectById(Integer id);
+
+    DataResponseBean<List<People>> findByName(String name);
+
+    SimpleResponseBean createPeopleIndex(String index, String aliases);
+
+    SimpleResponseBean deletePeopleIndex(String index, String id);
+
+    DataResponseBean<People> searchIndex(String index, String keyword);
+
+    SimpleResponseBean save(People people);
+
+    SimpleResponseBean saveList(List<People> people);
 }
